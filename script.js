763b333030fe721e236b7aff958fc806c9b49c9c
Patrickbdn <patrick.bodin.pro@gmail.com>
2024-06-18 14:16:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector('.scrollToTop').classList.add('show');
+  } else {
+    document.querySelector('.scrollToTop').classList.remove('show');
+  }
+}
+
+document.querySelector('.scrollToTop').addEventListener('click', function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const image = document.getElementById("rotateImage");
+
+  image.addEventListener("mouseover", () => {
+      image.style.transform = "rotateY(360deg)";
+  });
+
+  image.addEventListener("mouseout", () => {
+      image.style.transform = "rotateY(0deg)";
+  });
+});
